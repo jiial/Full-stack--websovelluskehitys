f1f05acc4125ad5a5b31a9e6f48ce0e8d1907061
jiial <jonelong96@gmail.com>
2018-02-12 23:35:17 +0200

    viikon 3 muutoksia

--- osa2/2_6/src/services/persons.js
+++ osa2/2_6/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -18,8 +18,7 @@ const update = (id, newObject) => {
 
 const remove = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
-    const request2 = axios.get(baseUrl)
-    return request2.then(response => response.data)
+    return request.then(response => response.data)
 }
 
 export default { getAll, create, update, remove }
